0701ce744d7449d16313ed33fe94d99f06fd4570
gihun Kim <hunjyhunji@gmail.com>
2025-05-20 23:41:33 +0900

    배포테스트

--- check-processes.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Checking processes using build files...
-for /f "tokens=4" %%p in ('handle -a "C:\DEV\autocoin\autocoin_spring_api\build"') do (
-    echo Process: %%p
-)
-echo Process check completed.
-pause
--- stop-gradle.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Stopping Gradle Daemon...
-cd C:\DEV\autocoin\autocoin_spring_api
-gradlew --stop
-echo Gradle Daemon stopped.
-pause
